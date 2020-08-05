013e6a2fa529de5cceab33ee10c4e30d17df936d
aamodpant15 <aamodpant15@gmail.com>
2020-08-05 23:49:24 +0530

    Updates

---
+++ precache-manifest.ea2fab16dc2d8f77ab8e87a7fee647a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c3b027745c34f6c2bcfc985ebf45d7e",
+    "url": "/pb-info/index.html"
+  },
+  {
+    "revision": "d8985397fd88c95975d9",
+    "url": "/pb-info/static/css/main.bfb0a378.chunk.css"
+  },
+  {
+    "revision": "b0c2b58471009cce5024",
+    "url": "/pb-info/static/js/2.c210b1a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pb-info/static/js/2.c210b1a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8985397fd88c95975d9",
+    "url": "/pb-info/static/js/main.e9395cdd.chunk.js"
+  },
+  {
+    "revision": "4aa01162c815970db7dd",
+    "url": "/pb-info/static/js/runtime-main.8c6fa064.js"
+  }
+]);
